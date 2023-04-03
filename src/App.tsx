bab3c4563964a1f9b542084286b7514f9cf2cbc2
YarullinRS <yarullinrs@aquilon-st.ru>
2023-04-03 10:23:07 +0300

    + app-burger-tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import './App.css';
 
+// data
+import {burgers} from './utils/data';
+
 // components
 import AppHeader from './components/app-header/app-header';
 import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
+import BurgerConstructor from './components/burger-constructor/burger-constructor';
 
 
 function App() {
     return (
         <>
             <AppHeader></AppHeader>
-            <main className='app-burger-paddings app-burger-main'>
+            <main className='app-burger-paddings app-burger-main pb-5 pt-5'>
                 <span className={`text text_type_main-large`}>
                     Соберите бургер
                 </span>
                 <br></br>
-                <BurgerIngredients />
+                <div className='app-burger-section'>
+                    <BurgerIngredients />
+                </div>
+                <div className='app-burger-section'>
+                    <BurgerConstructor burgers={burgers}></BurgerConstructor>
+                </div>
+                {/* <BurgerIngredients /> */}
             </main>
         </>
     );
