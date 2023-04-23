e73771b72c565497481239de52c99b464d98a809
dqrest <yarullinrs@gmail.com>
2023-04-23 06:38:06 +0300

    remove typescript type annotations

--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -2,7 +2,7 @@ import { checkResponse } from '../shared/utils/check-response';
 
 const NORMA_API = "https://norma.nomoreparties.space/api";
 
-export function getIngredientsRequest(): Promise<any> {
+export function getIngredientsRequest() {
     return fetch(`${NORMA_API}/ingredients`)
         .then(checkResponse)
 }
