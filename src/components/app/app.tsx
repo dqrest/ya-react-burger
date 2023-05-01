7dd900de48a20ccdaef5947b839d15d130270345
dqrest <yarullinrs@gmail.com>
2023-05-01 19:38:16 +0300

    add blank pages

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // pages
-import { HomePage, BurgerContructorPage } from '../../pages';
+import {
+    HomePage
+    , BurgerContructorPage
+    , LoginPage
+    , RegisterPage
+    , ForgotPasswordPage
+    , ResetPasswordPage
+    , ProfilePage
+    , IngredientPage
+} from '../../pages';
 
 
 export default function App() {
@@ -10,7 +19,13 @@ export default function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />}>
-                    <Route path="" element={<BurgerContructorPage/> }/>
+                    <Route path="" element={<BurgerContructorPage />} />
+                    <Route path="login" element={<LoginPage />} />
+                    <Route path="register" element={<RegisterPage />} />
+                    <Route path="forgot-password" element={<ForgotPasswordPage />} />
+                    <Route path="reset-password" element={<ResetPasswordPage />} />
+                    <Route path="profile" element={<ProfilePage />} />
+                    <Route path="ingredients/:id" element={<IngredientPage />} />
                 </Route>
             </Routes>
         </Router>        
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,20 @@
+// pages
 import { HomePage } from './home';
 import { BurgerContructorPage } from './burger-constructor';
+import { ForgotPasswordPage } from './forgot-password';
+import { LoginPage } from './login';
+import { ProfilePage } from './profile';
+import { RegisterPage } from './register';
+import { ResetPasswordPage } from './reset-password';
+import { IngredientPage } from './ingredient';
 
 export {
     HomePage
     , BurgerContructorPage
+    , ForgotPasswordPage
+    , LoginPage
+    , ProfilePage
+    , RegisterPage
+    , ResetPasswordPage
+    , IngredientPage
 };
