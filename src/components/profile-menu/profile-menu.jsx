598d01d5ee5cfb56704d7b7d15148b097c8c8679
dqrest <yarullinrs@gmail.com>
2023-05-02 00:10:27 +0300

    + tooltip

--- src/components/profile-menu/profile-menu.jsx
+++ src/components/profile-menu/profile-menu.jsx
@@ -6,37 +6,47 @@ import menuStyles from './profile-menu.module.css';
 
 const menuIems = [{
     title: 'Профиль'
-    , url: '/profile-user'
+    , url: '/profile/user'
     , key: 'profile-user'
+    , tooltip: 'В этом разделе Вы можете изменить свои персональные данные'
 }, {
     title: 'История заказов'
     , url: '/profile/orders'
     , key: 'profile-order'
+    , tooltip: 'В этом разделе Вы можете просмотреть свою историю заказов'
 }, {
     title: 'Выход'
     , url: '/logout'
     , key: 'logout'
+    , tooltip: 'Выйти из системы'
 }];
 
 export const ProfileMenu = () => {
 
     const [selectedItem, setSelectedItem] = useState();
-    useEffect(() => menuIems?.length > 1 && setSelectedItem(menuIems[1]?.key)
+    useEffect(() => menuIems?.length > 0 && setSelectedItem(menuIems[0])
         , []);
 
     return (
-        <ul className={menuStyles.list}>{menuIems.map((item) => (
-            <li key={item.key}
-                className={`text text_type_main-default ${menuStyles.item} ${menuStyles.item} ${selectedItem === item?.key && menuStyles.selected}`}
-                onClick={() => setSelectedItem(item?.key)}>
+        <>
+            <ul className={menuStyles.list}>
+                {menuIems.map((item) => (
+                    <li key={item.key}
+                        className={`text text_type_main-default ${menuStyles.item} ${menuStyles.item} ${selectedItem?.key === item?.key && menuStyles.selected}`}
+                        onClick={() => setSelectedItem(item)}>
 
-                <Link key={`link_${item.key}`}
-                    className={`${menuStyles.link}`}
-                    to={`${item.url}`}>
-                    {item.title}
-                </Link>
+                        <Link key={`link_${item.key}`}
+                            className={`${menuStyles.link}`}
+                            to={`${item.url}`}>
+                            {item.title}
+                        </Link>
 
-            </li>
-        ))}</ul>
+                    </li>
+                ))}
+            </ul>
+            <div className={`text text_type_main-small ${menuStyles.item} mt-15`}>
+                {selectedItem?.tooltip}
+            </div>
+        </>
     );
 }
