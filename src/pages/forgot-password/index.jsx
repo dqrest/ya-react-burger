580aacb76e60b8224a0b7d9291d3b1e414f110d5
dqrest <yarullinrs@gmail.com>
2023-05-01 20:58:40 +0300

    ForgotPasswordPage, ResetPasswordPage: add templates

--- src/pages/forgot-password/index.jsx
+++ src/pages/forgot-password/index.jsx
@@ -1,5 +1,28 @@
+import { Link } from 'react-router-dom';
+import {    
+    EmailInput
+    , Button
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
+// styles
+import styles from '../pages.module.css';
 
 export const ForgotPasswordPage = () => (
-	<>ForgotPasswordPage</>
+    <div className={styles.wrapper}>
+        <span className="text text_type_main-medium">
+            Восстановление пароля
+        </span>
+        <EmailInput placeholder="Укажите e-mail" />
+        <Button htmlType="button" type="primary" size="medium">
+            Восстановить
+        </Button>
+        <span className="text text_type_main-small mt-10">
+            Вспомнили пароль?
+            <Link
+                to={{ pathname: `/login` }}
+                className={`${styles.link} ml-4`}>
+                Войти
+            </Link>
+        </span>       
+    </div>
 );
--- src/pages/reset-password/index.jsx
+++ src/pages/reset-password/index.jsx
@@ -1,5 +1,30 @@
+import { Link } from 'react-router-dom';
+import {
+    PasswordInput
+    , Input
+    , Button
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
+// styles
+import styles from '../pages.module.css';
 
 export const ResetPasswordPage = () => (
-	<>ResetPasswordPage</>
+    <div className={styles.wrapper}>
+        <span className="text text_type_main-medium">
+            Восстановление пароля
+        </span>
+        <PasswordInput placeholder='Введите новый пароль' />
+        <Input placeholder='Введите код из письма' />
+        <Button htmlType="button" type="primary" size="medium">
+            Сохранить
+        </Button>
+        <span className="text text_type_main-small mt-10">
+            Вспомнили пароль?
+            <Link
+                to={{ pathname: `/login` }}
+                className={`${styles.link} ml-4`}>
+                Войти
+            </Link>
+        </span>
+    </div>
 );
