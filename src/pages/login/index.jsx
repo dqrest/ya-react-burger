0b7759d7ea7b2d7d59c29475a4ace54bb3852b17
dqrest <yarullinrs@gmail.com>
2023-05-01 20:18:03 +0300

    LoginPage: add template

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,39 @@
+import { Link } from 'react-router-dom';
+import {
+    PasswordInput
+    , EmailInput
+    , Button
+} from '@ya.praktikum/react-developer-burger-ui-components';
 
+
+// styles
+import loginStyles from './login.module.css';
 
 export const LoginPage = () => (
-	<>LoginPage</>
+    <div className={loginStyles.wrapper}>
+        <span className="text text_type_main-medium">
+            Вход
+        </span>
+        <PasswordInput />
+        <EmailInput />
+        <Button htmlType="button" type="primary" size="medium">
+            Войти
+        </Button>
+        <span className="text text_type_main-small mt-10">
+            Вы - новый пользователь?
+            <Link
+                to={{ pathname: `/register` }}
+                className={`${loginStyles.link} ml-4`}>
+                Зарегистрироваться
+            </Link>
+        </span>
+        <span className="text text_type_main-small">
+            Забыли пароль?
+            <Link
+                to={{ pathname: `/forgot-password` }}
+                className={`${loginStyles.link} ml-4`}>
+                Восстановить пароль
+            </Link>
+        </span>
+    </div>
 );
