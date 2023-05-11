734af9778d0c3267d04c9b052f3f9070362c23ab
YarullinRS <yarullinrs@aquilon-st.ru>
2023-05-11 14:03:28 +0300

    - dirty code

--- src/components/protected-route-element/protected-route-element.jsx
+++ src/components/protected-route-element/protected-route-element.jsx
@@ -1,15 +1,10 @@
-
-
 import { Navigate } from 'react-router-dom';
 
 export default function ProtectedRouteElement({ user, element }) {
-
-    debugger;
-
-    const pathname = document.location.pathname || '';
-
+    
     // nonauthorized user
     if (!user) {
+        const pathname = document.location.pathname || '';
 
         const fallback = pathname === '/login'
             ? '/'
@@ -23,25 +18,6 @@ export default function ProtectedRouteElement({ user, element }) {
             return element;        
 
         return <Navigate to={`/login${fallbackParam}`} />;
-    }
-
-    // switch(pathname){
-    //     case '/login':
-    //         return <Navigate to={``} />;       
-
-    // }
-
-
-    // authorized user
-    // switch (pathname) {
-    //     case '/login':
-    //     case '/register':
-    //     case '/forgot-password':
-    //     case '/reset-password':
-    //         // redirect to HomePage
-    //         return <Navigate to={``} />;
-    //     default:
-    //         return element;
-    // }
+    }    
     return element;
 }
