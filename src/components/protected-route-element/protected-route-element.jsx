c5e3f7668c42052c54d7e4a4fdaf2899c5dbee43
dqrest <yarullinrs@gmail.com>
2023-05-13 16:39:32 +0300

    protect '/forgot-password', '/reset-password', '/register' from the authorized user

--- src/components/protected-route-element/protected-route-element.jsx
+++ src/components/protected-route-element/protected-route-element.jsx
@@ -4,17 +4,17 @@ import PropTypes from 'prop-types';
 // shared
 import { userDto } from '../../shared/dtos/user-dto';
 
-export default function ProtectedRouteElement({ user, element }) {    
+export default function ProtectedRouteElement({ user, element }) {
 
     const location = useLocation();
-    const [searchParams, ] = useSearchParams();    
+    const [searchParams,] = useSearchParams();
     const pathname = location?.pathname || '';
-    let fb = searchParams.get('fallback');    
+    let fb = searchParams.get('fallback');
 
     // nonauthorized user
     if (!user) {
 
-        if (pathname.includes('/login') 
+        if (pathname.includes('/login')
             || pathname.includes('/forgot-password')
             || pathname.includes('/register'))
             return element;
@@ -39,10 +39,15 @@ export default function ProtectedRouteElement({ user, element }) {
     }
 
     // authorized user
-    if (pathname.includes('/login')) {
+    if (pathname.includes('/login')
+        || pathname.includes('/forgot-password')
+        || pathname.includes('/reset-password')
+        || pathname.includes('/register')    ) {
         return <Navigate to={fb || '/'} replace />;
     }
-    
+
+
+
     return element;
 }
 
