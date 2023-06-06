5f081b8d8772c31191c6a7962b5b76047942fa72
YarullinRS <yarullinrs@aquilon-st.ru>
2023-06-06 16:36:51 +0300

    typing actions

--- src/services/actions/ingredient-details.ts
+++ src/services/actions/ingredient-details.ts
@@ -1,7 +1,7 @@
 import { TBurgerIngredientsItemDto } from '../../shared/dtos/burger-ingredients-item-dto';
 
-export const SET_INGREDIENT_DETAILS = "SET_INGREDIENT_DETAILS";
-export const DELETE_INGREDIENT_DETAILS = "DELETE_INGREDIENT_DETAILS";
+export const SET_INGREDIENT_DETAILS: "SET_INGREDIENT_DETAILS" = "SET_INGREDIENT_DETAILS";
+export const DELETE_INGREDIENT_DETAILS: "DELETE_INGREDIENT_DETAILS" = "DELETE_INGREDIENT_DETAILS";
 
 export const setIngredientDetails = (ingredientItem?: TBurgerIngredientsItemDto | null) => (
     {
