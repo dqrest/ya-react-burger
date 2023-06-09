c14f156d411fd2d2bdd4a062de58c0cadbca1c74
YarullinRS <yarullinrs@aquilon-st.ru>
2023-06-09 13:40:23 +0300

    add ready template 'order-item'

--- src/components/order-item/order-item.tsx
+++ src/components/order-item/order-item.tsx
@@ -1,8 +1,10 @@
 import { FC, useMemo } from "react";
+import { v4 as uuid } from 'uuid';
 
-import { FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
+import { FormattedDate, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 // shared
+import { TBurgerIngredientsItemDto } from '../../shared/dtos/burger-ingredients-item-dto';
 import { TOrderItemDto } from '../../shared/dtos/order-item-dto';
 import { useSelector } from '../../services/hooks';
 import { useIngredients } from '../../services/selectors/burger-incredients';
@@ -13,7 +15,22 @@ import oStyle from './order-item.module.css';
 const OrderItem: FC<TOrderItem> = ({ order }) => {
 
     const { items: ingredients } = useSelector(useIngredients);
-    const orderIngredients = useMemo(() => ingredients.filter(ing => order.ingredients.includes(ing._id)), [order, ingredients]);
+    const groups = useMemo(() => {
+
+        const gs: TGroupedIngredients[] = [];
+        ingredients.forEach((ing, id) => {
+            const items = order.ingredients.filter(id => id === ing._id);
+            if (items.length === 0) return;
+            gs.push({
+                ingredient: ing,
+                count: items.length
+            });
+        });
+
+        return gs.sort((a, b) => a.count > b.count ? 1 : -1);
+    }, [order, ingredients]);
+
+    const total = useMemo(() => groups.map(g => g.ingredient.price * g.count).reduce((sum, currValue) => sum + currValue, 0), [groups]);
 
     return <div className={oStyle.orderItemWrapper}>
         <div className={`${oStyle.orderItem} p-5`}>
@@ -27,18 +44,34 @@ const OrderItem: FC<TOrderItem> = ({ order }) => {
                 </div>
             </div>
 
-            <p className="text text_type_main-medium">
+            <p className="text text_type_main-medium mt-4 mb-4">
                 {order.name}
             </p>
 
-            <div className={oStyle.ingredients}>
-                {orderIngredients.map(ing => (
-                    <div key={`oi_${ing._id}`}>
-                        <img src={ing.image_mobile}></img>                        
-                    </div>))
-                }
+
+            <div className={oStyle.body}>
+                <div className={oStyle.ingredients}>
+                    {groups.map((g, ind) =>
+                        <div key={`oi_${uuid()}`} className={oStyle.roundImageWrapper} style={{ zIndex: groups.length - ind, marginLeft: ind === 0 ? '0px' : '-20px' }}>
+
+                            {g.count > 1 &&
+                                <div className={`${oStyle.counter} ${oStyle.small} m-1 text text_type_digits-default`}>
+                                    <p className={`${oStyle.counter__num}`}>+{g.count}</p>
+                                </div>
+                            }
+
+                            <img key={uuid()} src={g.ingredient.image_mobile} className={oStyle.roundImage}>
+                            </img>
+                        </div>)
+                    }
+                </div>
+
+                <div className={`${oStyle.price} text text_type_digits-default`}>
+                    {total}
+                    &nbsp;
+                    <CurrencyIcon type="primary" />
+                </div>
             </div>
-            count: {orderIngredients.length}
         </div>
 
     </div>
@@ -47,5 +80,10 @@ const OrderItem: FC<TOrderItem> = ({ order }) => {
 type TOrderItem = {
     order: TOrderItemDto;
 }
+
+type TGroupedIngredients = {
+    ingredient: TBurgerIngredientsItemDto,
+    count: number;
+};
 
 export default OrderItem;
