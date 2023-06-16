9a9dbdfbbc15553088e188257524a5a9c27906ce
dqrest <yarullinrs@gmail.com>
2023-06-16 06:00:18 +0300

    fix bug: При открытии по прямой ссылке страницы с заказом - весь контент расползается по всей странице

--- src/pages/feed-item/feed-item.tsx
+++ src/pages/feed-item/feed-item.tsx
@@ -7,8 +7,13 @@ import OrderContent from '../../components/order-content/order-content';
 // shared
 import { WS_CONNECTION_START_TO_ALL_ORDERS } from '../../services/action-types/wsocket';
 
+// styles
+import style from './feed-item.module.css';
+
 export const FeedItemPage = () => {
-    return <OrderContent connection={WS_CONNECTION_START_TO_ALL_ORDERS} />
+    return (<div className={style.centerWrapper}>
+        <OrderContent connection={WS_CONNECTION_START_TO_ALL_ORDERS} />
+    </div>);
 };
 
 export const ModalFeedItemPage = () => {
